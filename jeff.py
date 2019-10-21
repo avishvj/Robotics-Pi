ddde2b6d7be18e597c9b1e6e5a92bf59296e1460
paris <paris.kawmai@gmail.com>
2019-10-21 18:12:30 +0100

    moves now with revolution instead of power

--- jeff.py
+++ jeff.py
@@ -8,7 +8,7 @@
 # For more information, see https://github.com/DexterInd/BrickPi3/blob/master/LICENSE.md
 #
 # This code is an example for running a motor to a target position set by the encoder of another motor.
-# 
+#
 # Hardware: Connect EV3 or NXT motors to the BrickPi3 motor ports B and C. Make sure that the BrickPi3 is running on a 9v power supply.
 #
 # Results:  When you run this program, motor C power will be controlled by the position of motor B. Manually rotate motor B, and motor C's power will change.
@@ -18,21 +18,36 @@ from __future__ import division       #                           ''
 
 import time     # import the time library for the sleep function
 import brickpi3 # import the BrickPi3 drivers
+import sys
+import math
 
 BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
 
-def turnRight():
-	start_posi = BP.get_motor_encoder(BP.PORT_C)
-	BP.set_motor_power(BP.PORT_C, 20)
-	BP.set_motor_power(BP.PORT_B, -20)
-	while (BP.get_motor_encoder(BP.PORT_C) - start_posi > 80):
+def turnRight(deg):
+	radius = 4.2 
+	circum = math.pi * 2 * 3.24
+	distance = radius * math.pi * (deg / 360) * 2
+	revolution = distance / circum * 360
+	start_posi = BP.get_motor_encoder(BP.PORT_A)
+	BP.set_motor_power(BP.PORT_A, 30)
+	BP.set_motor_power(BP.PORT_D, -30)
+	while (abs(BP.get_motor_encoder(BP.PORT_A) - start_posi) <  revolution):
 		print("turning")
-		print(BP.get_motor_encoder(BP.PORT_C) - start_posi)
-	stop()
+		print(abs(BP.get_motor_encoder(BP.PORT_A) - start_posi))
+
+def moveForward(cm):
+	radius = 3.24
+	circum = math.pi * 2 * radius
+	revolution = cm / circum * 360
+	start_posi = BP.get_motor_encoder(BP.PORT_A)
+	BP.set_motor_power(BP.PORT_A, 30)
+	BP.set_motor_power(BP.PORT_D, 30)
+	while (abs(BP.get_motor_encoder(BP.PORT_A) - start_posi) < revolution):
+		print("moving")
 
 def stop():
-	BP.set_motor_power(BP.PORT_C, 0)
-	BP.set_motor_power(BP.PORT_B, 0)
+	BP.set_motor_power(BP.PORT_A, 0)
+	BP.set_motor_power(BP.PORT_D, 0)
 	time.sleep(1)
 
 try:
@@ -40,11 +55,11 @@ try:
         BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B)) # reset encoder B
     except IOError as error:
         print(error)
-    
-    for i in range(4):
+
+    for i in range(12):
         # The following BP.get_motor_encoder function returns the encoder value (what we want to use to control motor C's power).
         try:
-            power = BP.get_motor_encoder(BP.PORT_B) / 10
+            power = BP.get_motor_encoder(BP.PORT_D) / 10
             if power > 100:
                 power = 100
             elif power < -100:
@@ -52,11 +67,12 @@ try:
         except IOError as error:
             print(error)
             power = 0
-        BP.set_motor_power(BP.PORT_C, 50)
-        BP.set_motor_power(BP.PORT_B, 50)
-        time.sleep(0.7)  # delay for 0.02 seconds (20ms) to reduce the Raspberry Pi CPU load.
+        #BP.set_motor_power(BP.PORT_A, 80)
+        #BP.set_motor_power(BP.PORT_D, 80)
+        #time.sleep(0.8)  # delay for 0.02 seconds (20ms) to reduce the Raspberry Pi CPU load.
+        moveForward(40)
         stop()
-        turnRight()
+        turnRight(int(sys.argv[1]))
         stop()
 
 except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
