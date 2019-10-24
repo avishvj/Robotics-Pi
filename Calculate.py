aac85e2edd82ed21fd1cbc6e2263abe2c8e496a0
Paris Mongkolkul <paris.kawmai@gmail.com>
2019-10-24 08:25:05 +0100

    update Calculate.py with real values

--- Calculate.py
+++ Calculate.py
@@ -14,8 +14,8 @@ def calculate_diagonal(x, y, x_mean, y_mean):
     return 1/N * np.sum(list(map(f, x, y)))
 
 
-x = [1, 2, 3, 4, 5, 6, 7, 8, 90, 10]
-y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+x = [0, 1.6, 1, 2.6, 3.9, -0.2, -0.8, -1.1, -3.7, -5.9]
+y = [1.4, 1.7, 2.4, 5.1, 5.1, 3.3, 2.9, 0.9, 0.9, 0.8]
 N = 10
 
 x_mean = calculate_mean(x)
