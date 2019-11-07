c45ebabff517a965bf5731ffa3a55becdd29f69d
paris <paris.kawmai@gmail.com>
2019-11-07 10:39:29 +0000

    update every files for cw2

--- second-assessment/My-Sonar.py
+++ second-assessment/My-Sonar.py
@@ -8,9 +8,9 @@
 # For more information, see https://github.com/DexterInd/BrickPi3/blob/master/LICENSE.md
 #
 # This code is an example for reading an NXT ultrasonic sensor connected to PORT_1 of the BrickPi3
-#
+# 
 # Hardware: Connect an NXT ultrasonic sensor to BrickPi3 Port 1.
-#
+# 
 # Results:  When you run this program, you should see the distance in CM.
 
 from __future__ import print_function # use python 3 syntax but make it compatible with python 2
@@ -19,7 +19,6 @@ from __future__ import division       #                           ''
 import time     # import the time library for the sleep function
 import brickpi3 # import the BrickPi3 drivers
 import sys
-import math
 
 BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
 
@@ -32,8 +31,8 @@ BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.NXT_ULTRASONIC)
 stopping_distance = 30
 
 def setPower(distance):
-	distance = distance - stopping_distance
-	if (distance > 100) :                                   # while the distance is above
+	distance = distance - stopping_distance                 
+	if (distance > 100) :                                   # while the distance is above 
 		BP.set_motor_power(BP.PORT_A, 75)
 		BP.set_motor_power(BP.PORT_D, 75)
 	elif (distance < 15 and distance > 0):                  # while robot is near object, slow it down.
@@ -43,29 +42,27 @@ def setPower(distance):
 		BP.set_motor_power(BP.PORT_A, distance * 0.75)
 		BP.set_motor_power(BP.PORT_D, distance * 0.75)
 
-def turnRight(deg):
+def turn(deg):
 	start_posi_d = BP.get_motor_encoder(BP.PORT_D)
 	start_posi_a = BP.get_motor_encoder(BP.PORT_A)
-	BP.set_motor_power(BP.PORT_A, 20)
-	BP.set_motor_power(BP.PORT_D, -20)
-	while (abs(BP.get_motor_encoder(BP.PORT_D) - start_posi_d) + abs(BP.get_motor_encoder(BP.PORT_A) - start_posi_a) < deg):
-		print("A status: ", BP.get_motor_status(BP.PORT_A), " D status: ", BP.get_motor_status(BP.PORT_D))
-	stop()
+	BP.set_motor_power(BP.PORT_A, -20)
+	BP.set_motor_power(BP.PORT_D, 20)
+	while (int((abs(BP.get_motor_encoder(BP.PORT_D) - start_posi_d) + abs(BP.get_motor_encoder(BP.PORT_A) - start_posi_a))) < deg):
+		a = 0
+	calculateRotation()
+		#print("A status: ", BP.get_motor_status(BP.PORT_A), " D status: ", BP.get_motor_status(BP.PORT_D))
 
 def moveForward(cm):
-	circum = 23
-	revolution = cm / 21 * 360
-	start_posi_D = BP.get_motor_encoder(BP.PORT_D)
-	start_posi_A = BP.get_motor_encoder(BP.PORT_A)
+	radius = 3.24
+	circum = math.pi * 2 * radius
+	revolution = cm / circum * 360 * 0.997
+	start_posi = BP.get_motor_encoder(BP.PORT_D)
 	BP.set_motor_power(BP.PORT_A, 30)
 	BP.set_motor_power(BP.PORT_D, 30)
-	while ((abs(BP.get_motor_encoder(BP.PORT_D) - start_posi_D) + abs(BP.get_motor_encoder(BP.PORT_A) - start_posi_A)) / 2 < revolution):
-		if (abs(BP.get_motor_encoder(BP.PORT_D)) % 6 == 0 and abs(BP.get_motor_encoder(BP.PORT_D)) % 9 == 0):
-			BP.set_motor_power(BP.PORT_D, 29)
-		elif (abs(BP.get_motor_encoder(BP.PORT_D)) % 6 == 0):
-			BP.set_motor_power(BP.PORT_D, 30)
-		print("A status: ", BP.get_motor_status(BP.PORT_A), " D status: ", BP.get_motor_status(BP.PORT_D))
-	stop()
+	while (abs(BP.get_motor_encoder(BP.PORT_D) - start_posi) < revolution):
+		a = 0
+	#	print("A status: ", BP.get_motor_status(BP.PORT_A), " D status: ", BP.get_motor_status(BP.PORT_D))
+	calculateStraightLine(cm)
 
 def stop():
 	BP.set_motor_power(BP.PORT_A, 0)
@@ -73,23 +70,14 @@ def stop():
 	time.sleep(1)
 
 try:
-        # read and display the sensor value
-        # BP.get_sensor retrieves a sensor value.
-        # BP.PORT_1 specifies that we are looking for the value of sensor port 1.
-        # BP.get_sensor returns the sensor value (what we want to display).
-#        for i in range(4):
-#            #value = BP.get_sensor(BP.PORT_1)
-#            #setPower(value)
-#
-#           for i in range(4):
-#              moveForward(int(sys.argv[2]) / 4)
-#              stop()
-            moveForward(40)
-            turnRight(int(sys.argv[1]))
-            moveForward(40)
-            #stop()
-#           turnRight(int(sys.argv[1]))
-#           stop()
+	while True:
+		try: 
+			value = BP.get_sensor(BP.PORT_1)
+			#setPower(value)
+			print(value)
+		except brickpi3.SensorError as error:
+			print(error)
+		time.sleep(0.02)
 
 except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
     BP.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
--- second-assessment/NXT-Ultrasonic_Sensor.py
+++ second-assessment/NXT-Ultrasonic_Sensor.py
@@ -20,6 +20,8 @@ import time     # import the time library for the sleep function
 import brickpi3 # import the BrickPi3 drivers
 import sys
 import math
+import numpy as np
+import random
 
 BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
 
@@ -30,6 +32,15 @@ BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be 
 BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.NXT_ULTRASONIC)
 
 stopping_distance = 30
+
+DISTANCE = 40
+E_ERROR = 0.01
+F_ERROR = 0.002
+G_ERROR = 0.01
+NUMBER_OF_PARTICLES = 10
+SCALING_FACTOR = 10
+particles = np.full((NUMBER_OF_PARTICLES, 3), (100, 100, 0), dtype=float)
+
 
 def setPower(distance):
 	distance = distance - stopping_distance                 
@@ -43,13 +54,64 @@ def setPower(distance):
 		BP.set_motor_power(BP.PORT_A, distance * 0.75)
 		BP.set_motor_power(BP.PORT_D, distance * 0.75)
 
-def turnRight(deg):
+def printLine():
+	global particles, SCALING_FACTOR, DISTANCE
+	x = particles[0][0]
+	y = particles[0][1]
+	scaledDistance = DISTANCE * SCALING_FACTOR
+	line1 = (x, y, x + scaledDistance, y)
+	line2 = (x + scaledDistance, y, x + scaledDistance, y + scaledDistance)
+	line3 = (x + scaledDistance, y + scaledDistance, x, y + scaledDistance)
+	line4 = (x, y + scaledDistance, x, y)
+	print("drawLine:" + str(line1))
+	print("drawLine:" + str(line2))
+	print("drawLine:" + str(line3))
+	print("drawLine:" + str(line4))
+	
+
+def printParticles():
+	global particles
+	particlesTuples = ""
+	for i in range(len(particles)):
+		particlesTuples += str(tuple(particles[i]))
+	print("drawParticles:" + particlesTuples)
+
+def calculateStraightLine(D):
+	global particles, E_ERROR, F_ERROR, SCALING_FACTOR
+	for i in range(len(particles)):
+		e = random.gauss(0, E_ERROR)
+		f = random.gauss(0, F_ERROR)
+
+		x = particles[i][0]
+		y = particles[i][1]
+		theta = particles[i][2]
+
+		particles[i] = (x + (D + e) * math.cos(theta) * SCALING_FACTOR, y + (D + e) * math.sin(theta) * SCALING_FACTOR, theta + f)
+		tuple = (particles[i][0], particles[i][1], particles[i][2])
+	printParticles()
+
+def calculateRotation():
+	global particles, G_ERROR
+	for i in range(len(particles)):
+		g = random.gauss(0, G_ERROR)
+
+		x = particles[i][0]
+		y = particles[i][1]
+		theta = particles[i][2]
+
+		particles[i] = (x, y, theta + (math.pi / 2) + g)
+		tuple = (particles[i][0], particles[i][1], particles[i][2])
+	printParticles()
+
+def turn(deg):
 	start_posi_d = BP.get_motor_encoder(BP.PORT_D)
 	start_posi_a = BP.get_motor_encoder(BP.PORT_A)
 	BP.set_motor_power(BP.PORT_A, -20)
 	BP.set_motor_power(BP.PORT_D, 20)
 	while (int((abs(BP.get_motor_encoder(BP.PORT_D) - start_posi_d) + abs(BP.get_motor_encoder(BP.PORT_A) - start_posi_a))) < deg):
-		print("A status: ", BP.get_motor_status(BP.PORT_A), " D status: ", BP.get_motor_status(BP.PORT_D))
+		a = 0
+	calculateRotation()
+		#print("A status: ", BP.get_motor_status(BP.PORT_A), " D status: ", BP.get_motor_status(BP.PORT_D))
 
 def moveForward(cm):
 	radius = 3.24
@@ -59,7 +121,9 @@ def moveForward(cm):
 	BP.set_motor_power(BP.PORT_A, 30)
 	BP.set_motor_power(BP.PORT_D, 30)
 	while (abs(BP.get_motor_encoder(BP.PORT_D) - start_posi) < revolution):
-		print("A status: ", BP.get_motor_status(BP.PORT_A), " D status: ", BP.get_motor_status(BP.PORT_D))
+		a = 0
+	#	print("A status: ", BP.get_motor_status(BP.PORT_A), " D status: ", BP.get_motor_status(BP.PORT_D))
+	calculateStraightLine(cm)
 
 def stop():
 	BP.set_motor_power(BP.PORT_A, 0)
@@ -71,16 +135,19 @@ try:
         # BP.get_sensor retrieves a sensor value.
         # BP.PORT_1 specifies that we are looking for the value of sensor port 1.
         # BP.get_sensor returns the sensor value (what we want to display).
+        printParticles()
+        printLine()
+        step = DISTANCE / 4
         for i in range(4):
             #value = BP.get_sensor(BP.PORT_1)
             #setPower(value)
 
            for i in range(4):
-              moveForward(int(sys.argv[2]) / 4)
+              moveForward(step)
               stop()
             #moveForward(37)
             #stop()
-           turnRight(int(sys.argv[1]))
+           turn(300)
            stop()
 
 except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
